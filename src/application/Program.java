76b9e4ddde08fb7a956212f7cde0246ac15c4ce8
Alexsandro Rodrigues <alex.rsjr10@gmail.com>
2023-01-07 18:31:10 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,8 @@ public class Program {
         
         Department department = new Department(1, "Books");
         Seller seller = new Seller(42, "Bob Braw", "bob@gmail.com", new Date(), 4600.00, department);
+        SellerDao sd = DaoFactory.createSellerDao();
+        sd.insert(seller);
         System.out.println(seller);
     }
 }

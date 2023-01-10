00d49f0be62484abea4600117652a38a63996a8d
Alexsandro Rodrigues <alex.rsjr10@gmail.com>
2023-01-09 22:03:13 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -29,5 +29,11 @@ public class Program {
         Seller newSeller = new Seller(null, "João Pereira", "joaop@gmail.com", new Date(), 3060.00, new Department(2, "Electronics"));
         sd.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n==== TEST 4: Seller insert ====");
+        seller = sd.findById(5);
+        seller.setBaseSalary(8000.00);
+        sd.update(seller);
+        System.out.println("Update completed!");
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -56,6 +56,28 @@ public class SellerDaoJDBC implements SellerDao{
             DB.closeStatement(ps);
         }
     }
+ 
+    @Override
+    public void update(Seller obj) {
+        PreparedStatement ps = null;
+        try{
+            ps = conn.prepareStatement(
+            "UPDATE seller "+
+            "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "+
+            "WHERE Id = ?");
+            ps.setString(1, obj.getName());
+            ps.setString(2, obj.getEmail());
+            ps.setDate(3, new Date(obj.getBirthDate().getTime()));
+            ps.setDouble(4, obj.getBaseSalary());
+            ps.setInt(5, obj.getDepartment().getId());
+            ps.setInt(6, obj.getId());
+            ps.executeUpdate();
+        }catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }finally{
+            DB.closeStatement(ps);
+        }           
+    }
 
     @Override
     public void delete(Seller obj) {
